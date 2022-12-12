74b540e8559cd2606253bd44d766d5cc06f35ef7
nofar.av <nofar.av@campus.technion.ac.il>
2022-12-12 15:25:14 +0200

    q3-7

--- main.py
+++ main.py
@@ -42,7 +42,81 @@ for index, k in enumerate(k_values):
   validation_scores[index] = results["test_score"].mean()
 
   
+
+g = plt.semilogx(k_values, train_scores, color = 'orange', markersize = 15)
+g = plt.semilogx(k_values, validation_scores, color = 'teal', markersize = 15)
+
+plt.xlabel('k')
+plt.ylabel('score')
+plt.title('Cross Validation Train and Validation Scores (PCR_01 & PCR_02)')
+plt.grid(True)
+plt.legend(["train", "validation"], loc ="upper right")
+plt.show()
+
+
+#Q4
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread)
+visualize_clf(knn, DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread, "kNN Model (k = 9)", "PCR_01", "PCR_02")
+
+norm_test = prepare_data(train, test)
+X_test = norm_test[["PCR_01", "PCR_02"]]
+y_test = norm_test.spread
+print("test score: " + str(knn.score(X_test, y_test)))
+
+#Q5
+from sklearn.neighbors import KNeighborsClassifier
+
+for k in [1, 501]:
+  knn = KNeighborsClassifier(n_neighbors=k)
+  knn.fit(DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread)
+  visualize_clf(knn, DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread, "kNN Model (k = " + str(k) + ")", "PCR_01", "PCR_02")
+
+norm_test = prepare_data(train, test)
+X_test = norm_test[["PCR_01", "PCR_02"]]
+y_test = norm_test.spread
+print("test score: " + str(knn.score(X_test, y_test)))
+
+#model selection - Q6
+from sklearn.model_selection import cross_validate
+
+k_values  = list(range(1, 20, 2)) + list(range(20, 871, 85))
+validation_scores= np.empty(shape=len(k_values), dtype=float)
+train_scores = np.empty(shape=len(k_values), dtype=float)
+
+for index, k in enumerate(k_values):
+  knn = KNeighborsClassifier(n_neighbors=k)
+  knn.fit(norm_train, norm_train.spread)
+  results = cross_validate(knn, norm_train, y=norm_train.spread, cv=8,  return_train_score=True)
+  train_scores[index] = results["train_score"].mean()
+  validation_scores[index] = results["test_score"].mean()
+
+  
 print(train_scores)
 print(validation_scores)
-#plt.semilogx(train_scores, k_values)
-plt.semilogx(validation_scores, k_values)
+
+g = plt.semilogx(k_values, train_scores, color = 'orange', markersize = 15)
+g = plt.semilogx(k_values, validation_scores, color = 'teal', markersize = 15)
+
+plt.xlabel('k')
+plt.ylabel('score')
+plt.title('Cross Validation Train and Validation Scores')
+plt.grid(True)
+plt.legend(["train", "validation"], loc ="lower left")
+plt.show()
+
+
+#Q7 decision trees  
+#TODO::AN UGLY LOOKING TREE
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+decision_tree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+decision_tree.fit(norm_train.drop(["risk","spread"], inplace= False, axis = 1), norm_train.risk)
+trainAcc = np.sum(decision_tree.predict(norm_train.drop(["risk","spread"], inplace= False, axis = 1)) == norm_train.risk) * 100 / len(norm_train.risk)
+# normalized_train = norm_test.drop(["risk","spread"], inplace= False, axis = 1)
+print("train accuracy is "+ str(trainAcc))
+
+plt.figure(figsize=(26,12))
+plot_tree(decision_tree, feature_names=norm_train.columns, filled=True, fontsize=10)
