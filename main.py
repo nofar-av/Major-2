b98828bae19382eb0d9477608d3ad08e53ba9de3
Omer <Omerbenishay@campus.technion.ac.il>
2022-12-17 10:45:03 +0200

    Q11 - Q18

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread)
-visualize_clf(knn, DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread, "kNN Model", "PCR_01", "PCR_02")
+visualize_clf(knn, DataFrame[["PCR_01", "PCR_02"]], DataFrame.spread, "kNN Model(k=1)", "PCR_01", "PCR_02")
 
 
 #model selection - Q3
@@ -173,3 +173,94 @@ print("test accuracy is:", decision_tree.score(norm_test.drop(["risk","spread"],
 X_train = np.array(norm_train[["PCR_01","PCR_02"]])
 y_train = np.array(norm_train.spread)
 compare_gradients(X_train, y_train, deltas=np.logspace(-9, -1, 12))
+
+#Q11
+for lr in np.logspace(-3, 0, 4):
+  clf = SoftSVM(C=0.1, lr=lr)
+  losses, accuracies = clf.fit_with_logs(X_train, y_train, max_iter=3000)
+  plt.figure(figsize=(15, 8))
+  plt.subplot(121), plt.grid(alpha=0.5), plt.title ("losses with lr=" + str(lr))
+  plt.semilogy(losses), plt.xlabel("iteration"), plt.ylabel("loss")
+  plt.subplot(122), plt.grid(alpha=0.5), plt.title ("accuracies with lr=" + str(lr))
+  plt.plot(accuracies), plt.xlabel("iteration"), plt.ylabel("accuracy")
+
+#Q12
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+C = 1e13
+svm_2nd = Pipeline([('feature_mapping', PolynomialFeatures(2)), ('scaler', StandardScaler()), ('SVM', SoftSVM(C=C, lr=(1/(C*5e2))))])
+svm_2nd.fit(X_train, y_train, SVM__max_iter=10000)
+svm_3rd = Pipeline([('feature_mapping', PolynomialFeatures(3)), ('scaler', StandardScaler()), ('SVM', SoftSVM(C=C, lr=(1/(C*5e2))))])
+svm_3rd.fit(X_train, y_train, SVM__max_iter=10000)
+
+print("Train and test score 2nd deg: ", svm_2nd.score(X_train, y_train), svm_2nd.score(X_test, y_test))
+print("Train and test score 2nd deg: ", svm_3rd.score(X_train, y_train), svm_3rd.score(X_test, y_test))
+visualize_clf(svm_2nd, X_train, y_train, "Poly Model (2nd degree)", "PCR_01", "PCR_02")
+visualize_clf(svm_3rd, X_train, y_train, "Poly Model (3rd degree)", "PCR_01", "PCR_02")
+
+#Q13
+train_acc = []
+for i in range(5):
+  svm_3rd = Pipeline([('feature_mapping', PolynomialFeatures(3)), ('scaler', StandardScaler()), ('SVM', SoftSVM(C=C, lr=(1/(C*5e2))))])
+  svm_3rd.fit(X_train, y_train, SVM__max_iter=10000)
+  train_acc.append(svm_3rd.score(X_train, y_train))
+  visualize_clf(svm_3rd, X_train, y_train, "3rd Degree Poly Model (i=" + str(i) + ")", "PCR_01", "PCR_02")
+  print("Train accuracy: ", train_acc[i])
+print("mean and std: ", np.mean(train_acc), np.std(train_acc))
+
+#Q15
+from sklearn.svm import SVC
+C = 1e4
+for gamma in np.logspace(-5, 3, 9):
+  clf = SVC(C=C, kernel="rbf", gamma=gamma)
+  clf.fit(X_train, y_train)
+  visualize_clf(clf, X_train, y_train, "SVC Model(RBF kernel) with gamma=" + str(gamma), "PCR_01", "PCR_02", marker_size=50)
+  print("train and test accuracy ", clf.score(X_train, y_train), clf.score(X_test, y_test))
+
+clf = SVC(C=C, kernel="rbf", gamma=1e4)
+clf.fit(X_train, y_train)
+visualize_clf(clf, X_train, y_train, "SVC Model(RBF kernel) with gamma=" + str(1e4), "PCR_01", "PCR_02", marker_size=10)
+print("train and test accuracy ", clf.score(X_train, y_train), clf.score(X_test, y_test))
+
+#Q17
+from sklearn.model_selection import cross_validate
+
+# gamma_values  = np.logspace(-1, 1, 14)
+gamma_values  = np.linspace(1.5, 3, 16)
+validation_scores= np.empty(shape=len(gamma_values), dtype=float)
+train_scores = np.empty(shape=len(gamma_values), dtype=float)
+C = 1e4
+for index, gamma in enumerate(gamma_values):
+  clf = SVC(C=C, kernel="rbf", gamma=gamma)
+  clf.fit(X_train, y_train)
+  results = cross_validate(clf, X_train, y=y_train, cv=8,  return_train_score=True)
+  train_scores[index] = results["train_score"].mean()
+  validation_scores[index] = results["test_score"].mean()
+
+  
+
+# g = plt.semilogx(gamma_values, train_scores, color = 'orange', markersize = 15)
+# g = plt.semilogx(gamma_values, validation_scores, color = 'teal', markersize = 15)
+g = plt.plot(gamma_values, train_scores, color = 'orange', markersize = 15)
+g = plt.plot(gamma_values, validation_scores, color = 'teal', markersize = 15)
+
+
+plt.xlabel('gamma')
+plt.ylabel('score')
+plt.title('SVC Model(RBF kernel) Cross Validation Train and Validation Scores(PCR_01 & PCR_02)')
+plt.grid(True)
+plt.legend(["train", "validation"], loc ="upper right")
+plt.show()
+
+print(gamma_values)
+print("best gamma is: ", gamma_values[np.argmax(validation_scores)])
+print("its validation score and train score: ", validation_scores[np.argmax(validation_scores)], train_scores[np.argmax(validation_scores)])
+
+#Q18
+best_gamma = 2.3
+C=1e4
+clf = SVC(C=C, kernel="rbf", gamma=best_gamma)
+clf.fit(X_train, y_train)
+print("Test accuracy: ",clf.score(X_test, y_test))
+visualize_clf(clf, X_train, y_train, "SVC Model(RBF kernel) with gamma=" + str(best_gamma), "PCR_01", "PCR_02", marker_size=50)
